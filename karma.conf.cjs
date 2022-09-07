0aa5befde148bd956a73efd45dda12678e7bcb25
Ganesh Annan <gannan@digitalbazaar.com>
2022-09-08 01:06:11 +0200

    Update webpack conf for karma.

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -29,7 +29,10 @@ module.exports = function(config) {
 
     webpack: {
       mode: 'development',
-      devtool: 'inline-source-map'
+      devtool: 'inline-source-map',
+      experiments: {
+        topLevelAwait: true,
+      },
     },
 
     // test results reporter to use
